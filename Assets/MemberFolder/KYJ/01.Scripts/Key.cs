f472c0cb6dc9a30251e0f1e278d0a6a1e6c5a4bd
Yoonji21 <dayen.com.nate@gmail.com>
2024-10-15 00:44:05 +0900

    fix : Key

--- Assets/MemberFolder/KYJ/01.Scripts/Key.cs
+++ Assets/MemberFolder/KYJ/01.Scripts/Key.cs
@@ -1,45 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using 
+using DG.Tweening;
 
 public class Key : MonoBehaviour
 {
-    private GameObject _key;
-    private GameObject _keyHole;
     private GameObject _barrier;
-    private Transform _keyPosition;
+    [SerializeField] private Transform _playerTrm;
 
-    private float _keySpeed = 5f;
     public bool hasKey;
     private bool isHoleHit;
 
-    private void Awake()
-    {
-        _checkPlayer.OnPlayerCollide += HandlePlayerCollide;
-    }
 
-    private void Update()
-    {
-        if (isHoleHit && hasKey)
-        {
-            if (Input.GetKeyDown(KeyCode.F))
-                OnKeyEnter();
-        }
-    }
 
-    private void HandlePlayerCollide(string name)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (name == "Key")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            _key.transform.DOScale(0.5f, 0.5f);
+            gameObject.transform.SetParent(_playerTrm);
+            transform.DOScale(0.5f, 0.5f);
             hasKey = true;
         }
-        else if (name == "KeyHole")
-        {
-            isHoleHit = true;
-        }
     }
+
 
     private void FixedUpdate()
     {
@@ -51,15 +34,15 @@ public class Key : MonoBehaviour
 
     private void FollowPlayer()
     {
-        _key.transform.position = Vector3.Lerp
-            (_key.transform.position, _keyPosition.position, 0.05f);
+        transform.position = Vector3.Lerp
+            (transform.position, _playerTrm.position, 0.05f);
     }
 
     private void OnKeyEnter()
     {
         hasKey = false;
 
-        _key.gameObject.SetActive(false);
+        gameObject.SetActive(false);
         _barrier.gameObject.SetActive(false);
     }
 }
--- Assets/MemberFolder/KYJ/01.Scripts/OpenDoor.cs
+++ Assets/MemberFolder/KYJ/01.Scripts/OpenDoor.cs
@@ -2,7 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenDoor : Key
+public class OpenDoor : MonoBehaviour
 {
-    
+    private Key _key;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Key key = collision.gameObject.GetComponent<Key>();
+
+        if (key != null)
+        {
+            if (key.hasKey)
+            {
+                print("¹® ¿­¸²");
+                key.hasKey = false;
+            }
+            else
+            {
+                print("¹® ´ÝÈû");
+            }
+        }
+        else
+        {
+            print("³Î");
+        }
+    }
+
+    private void KeyCheck()
+    {
+        if (_key.hasKey)
+        {
+            print("¹® ¿­¸²");
+        }
+        else
+        {
+            print("¹® ´ÝÈû");
+        }
+    }
 }
