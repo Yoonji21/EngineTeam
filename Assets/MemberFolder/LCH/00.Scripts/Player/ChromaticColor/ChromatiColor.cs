7472021ba3c1b96ceb53e99e46efa2fe3ffa789a
dlckdgh0414 <ci5976547@gmail.com>
2024-10-25 00:10:47 +0900

    feat : PushObj

--- Assets/MemberFolder/LCH/00.Scripts/Player/ChromaticColor/ChromatiColor.cs
+++ Assets/MemberFolder/LCH/00.Scripts/Player/ChromaticColor/ChromatiColor.cs
@@ -5,4 +5,24 @@ using UnityEngine;
 public class ChromatiColor : MonoBehaviour
 {
 
+    private GameObject[] _invisibleObjs;
+
+    private void Awake()
+    {
+        _invisibleObjs = GameObject.FindGameObjectsWithTag("InvisibleObj");
+
+    }
+
+    private void OnEnable()
+    {
+     
+        for (int i = 0; i < _invisibleObjs.Length; i++)
+            _invisibleObjs[i].SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < _invisibleObjs.Length; i++)
+            _invisibleObjs[i].SetActive(true);
+    }
 }
