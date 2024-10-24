33ff160b4b42fe38dae3e4b62c1bb82785018ab1
Yoonji21 <dayen.com.nate@gmail.com>
2024-10-24 09:23:33 +0900

    fix : Laser

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Laser : MonoBehaviour
 {
+    #region
     [SerializeField] private float def;
     public Transform laserPoint;
     public LineRenderer linerenderer;
-    private Transform _transform;
 
     public LayerMask target;
+    public LayerMask wall;
+
+    private Transform m_transform;
 
     [SerializeField] private Respawn respawn;
 
+
     private void Awake()
     {
-        _transform = GetComponent<Transform>();
+        m_transform = GetComponent<Transform>();
     }
 
     private void Update()
@@ -23,20 +28,37 @@ public class Laser : MonoBehaviour
         ShootLaser();   
     }
 
-    private bool DrawwRaw()
+    private bool DrawwRaw(out RaycastHit2D hit)
     {
-        bool isPlayer = Physics2D.Raycast(transform.position, transform.right, target);
-        return isPlayer;
+        // 플레이어와 벽 레이어마스크 설정 (레이어 이름은 Unity에서 설정된 이름을 사용하세요)
+        int layerMask = LayerMask.GetMask("Player", "Wall");
+
+        // 플레이어와 벽을 감지하는 Raycast
+        hit = Physics2D.Raycast(m_transform.position, transform.right, 10, layerMask);
+
+        // 레이저가 플레이어나 벽에 닿았는지 여부 반환
+        return hit.collider != null;
     }
 
     private void ShootLaser()
     {
-        if (DrawwRaw())
+        RaycastHit2D hit;
+
+        // 레이저가 플레이어나 벽에 닿았을 때
+        if (DrawwRaw(out hit))
         {
-            respawn.RespawnObject(true);
+            if (hit.collider.CompareTag("Player"))
+            {
+                // 플레이어를 맞췄을 때 리스폰 처리
+                respawn.RespawnObject(true);
+            }
+
+            // 레이저를 닿은 지점까지 그리기
+            Draw2DRay(laserPoint.position, hit.point);
         }
         else
         {
+            // 아무것도 맞지 않았을 경우 기본 방향으로 레이저 그리기
             Draw2DRay(laserPoint.position, laserPoint.transform.right * def);
         }
     }
@@ -46,4 +68,7 @@ public class Laser : MonoBehaviour
         linerenderer.SetPosition(0, startPos);
         linerenderer.SetPosition(1, endPos);
     }
+
+    #endregion
+
 }
