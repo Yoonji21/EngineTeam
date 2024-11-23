0b48ededc1b41cdad1c6b71b9a36f4872c69e9a9
dlckdgh0414 <ci5976547@gmail.com>
2024-11-23 17:06:11 +0900

    fix : Stages

--- Assets/MemberFolder/LCH/00.Scripts/UI/SettingUI.cs
+++ Assets/MemberFolder/LCH/00.Scripts/UI/SettingUI.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 public class SettingUI : MonoBehaviour
 {
 
-    [SerializeField] private GameObject _mainUI;
+    [SerializeField] private GameObject _settingUI;
     [SerializeField] private StageDataSO _dataSO;
-
-    private void Awake()
-    {
-        gameObject.SetActive(false);
-    }
 
     public void BackBtn()
     {
-        gameObject.SetActive(false);
-        _mainUI.SetActive(true);
+        _settingUI.SetActive(false);
     }
 
     public void GameReSet()
